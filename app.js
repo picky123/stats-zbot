6f96b8c87e62306c61afcfe8b452588be43b2ff4
picky123 <38593029+picky123@users.noreply.github.com>
2018-05-09 18:06:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = 'NDQzNzc5NjUwODk2Nzg5NTA0.DdSVhg.v4ALCCw3s1xRwraZBtYjZ-ePzIE'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login('NDQzNzc5NjUwODk2Nzg5NTA0.DdSVhg.v4ALCCw3s1xRwraZBtYjZ-ePzIE');
+
+client.on('ready', () => {
+  client.user.setGame('💀 Stats-Servers.', 'https://www.twitch.tv/streamerhouse')
+})
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('443419618724413452').setName(`Members: ${member.guild.memberCount}`);
+});
+
+client.on('guildMemberRemove', member => {
+    member.guild.channels.get('443419618724413452').setName(`Members: ${member.guild.memberCount}`);
+});
